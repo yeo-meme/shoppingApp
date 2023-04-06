191e3dd9fb692fe3105ee2bef2b0e5f7e4341c60
yeomim kim <yeomeme@gmail.com>
2023-04-06 14:54:31 +0900

    [add] detail 상품페이지 View
    -dummy 데이터

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,7 +1,9 @@
 import React, {useEffect} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {HomeStackParamList} from '../types';
+import { ScrollView } from 'react-native-gesture-handler';
+import images from '../constants/images';
 
 type DetailScreenNavigationProp = StackNavigationProp<
   HomeStackParamList,
@@ -20,15 +22,101 @@ const Detail: React.FC<Props> = ({navigation}) => {
   },[]);
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.backButton}
-        onPress={() => {
-          navigation.goBack();
-        }}>
-        <Text style={styles.backText}>Back</Text>
-      </TouchableOpacity>
-      <Text style={styles.text}>Detail Screen</Text>
+    <View style={{height: '100%', width: '100%', backgroundColor:'#fafafa'}}>
+      <ScrollView>
+
+<View style={{height: 500, padding: 2}}>
+
+<Image
+ source={images.nikeMetcon3}
+ resizeMode="contain"
+ style = {{
+     width: 400,
+     height: 600,
+ }}
+/>
+</View>
+
+<View 
+style={{
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-end',
+  margin: 10,
+}}>
+  <View style={{flex: 1, alignItems: 'flex-start'}}>
+  <Text>나이키</Text>
+  </View>
+  <View style={{flex: 1, alignItems: 'flex-end'}}>
+  <Text>200,000</Text>
+  </View>
+</View>
+
+<View
+style={{
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-end',
+  margin: 10,
+}}>
+ <View style={{flex: 1, alignItems: 'flex-start'}}>
+            <Text>
+              short description
+            </Text>
+          </View>
+          <View style={{flex: 1, alignItems: 'flex-end'}}>
+            <Text>(40%)</Text>
+          </View>
+        </View>
+      </ScrollView>
+
+
+<View
+style={{
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  width: '100%',
+  position: 'relative',
+  bottom: 0,
+  height: 70,
+  justifyContent: 'center',
+  backgroundColor: '#fff',
+}}>
+  <View
+  style={{
+    flex: 1,
+    backgroundColor: '#fafafa',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }}>
+    <TouchableOpacity
+    onPress={() => {
+      navigation.navigate('Cart');
+    }}>
+      <Text>ADD TO BAG</Text>
+    </TouchableOpacity>
+  </View>
+  <View
+          style={{
+            flex: 1,
+            backgroundColor: '#DA1C4C',
+            height: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Cart');
+            }}>
+            <Text style={{ color: '#ffffff'}}>
+              구매하기 
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+</View>
     </View>
   );
 };
