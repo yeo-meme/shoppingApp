8d96abb78de092f72ffe8a36072884a28963cf2e
yeomim kim <yeomeme@gmail.com>
2023-04-13 00:03:35 +0900

    [add] assets 추가

---
+++ constants/event_image.js
@@ -0,0 +1,10 @@
+export const coupon1 = require('../assets/snaps/coupon1.jpeg');
+export const coupon2 = require('../assets/snaps/coupon2.jpeg');
+export const event_banner = require('../assets/snaps/event1.jpeg');
+
+
+export default {
+    coupon1,
+    coupon2,
+    event_banner
+};
---
+++ constants/snap_image.js
@@ -0,0 +1,15 @@
+export const snap1 = require('../assets/snaps/snap1.jpg');
+export const snap2 = require('../assets/snaps/snap2.jpg');
+export const snap3 = require('../assets/snaps/snap3.jpg');
+export const snap4 = require('../assets/snaps/snap4.jpg');
+export const snap5 = require('../assets/snaps/snap5.jpg');
+
+
+
+export default {
+  snap1,
+  snap2,
+  snap3,
+  snap4,
+  snap5
+};
